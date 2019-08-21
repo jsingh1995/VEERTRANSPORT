111d6d844c4e46d92e3b54f57446fcecbdb7942d
jsingh1995 <46855377+jsingh1995@users.noreply.github.com>
2019-08-21 14:40:36 -0400

    Add files via upload

---
+++ formpage.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<!--
+Template Name: Boguco
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html>
+<head>
+<title>VEER TRANSPORT INC.</title> 
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="./layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<link rel="stylesheet" href="form.css" >
+<script src="form.js"></script>
+
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear"> 
+    
+<!-- ################################################################################################ -->
+    
+    <div class="fl_left">
+      <ul class="nospace">
+        <li><i class="fa fa-phone"></i> (317)534-2628 </li>
+        <li><i class="fa fa-envelope-o"></i> gary@veertransport.com </li>
+      </ul>
+    </div>
+    
+      <div class="fl_right">
+      <ul class="nospace">
+        <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="#footer">Contact</a></li>
+        <li><a href="https://intelliapp.driverapponline.com/c/277514?uri_b=ia_277514_1476142778">Apply</a></li> 
+     <!--   <li><a href="#">Thing</a></li> -->
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay" style="background-image:url('./images/demo/backgrounds/02.png');"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+
+      <div id="logo" class="fl_left">
+        <h1><a href="index.html"><img src="./images/veerlogo.png" style="width:300px;height:200px;" hspace="2"></a></h1>
+      </div>
+
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="index.html">Home</a></li>
+           <li><a href="equipment.html">Equipment</a></li>
+           <li><a href="about.html">About</a></li>  
+          <li><a href="formpage.html">Quote</a></li>
+          <li><a href="https://intelliapp.driverapponline.com/c/277514?uri_b=ia_277514_1476142778">Application</a></li>
+        </ul>
+      </nav>
+      <!-- ############################"pages/gallery.html"#################################################################### -->
+    </header>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- <div class="wrapper row2">
+    <div id="breadcrumb" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+  <!--     <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Ipsum</a></li>
+        <li><a href="#">Dolor</a></li>
+      </ul>
+      <!-- ################################################################################################ -->
+  <!--   </div>
+  </div>-->
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Top Background Image Wrapper -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ##########################<p>Dapiensociis <a href="#">temper donec auctortortis cumsan</a> et curabitur condis lorem loborttis leo. ###################################################################### -->
+<div class="col-md-6 col-md-offset-3">
+                    <h2>Contact Us</h2> 
+                    <p> Get A Quote </p>
+                    
+<!-- Start code for the form-->
+<form method="post" name="myemailform" action="form-to-email.php">
+	<p>
+		<label for='name'>Enter Name: </label><br>
+		<input type="text" name="name">
+	</p>
+	<p>
+		<label for='email'>Enter Email Address:</label><br>
+		<input type="text" name="email">
+	</p>
+	<p>
+		<label for='message'>Enter Message:</label> <br>
+		<textarea name="message"></textarea>
+	</p>
+	<input type="submit" name='submit' value="submit">
+</form>
+<script language="JavaScript">
+// Code for validating the form
+// Visit http://www.javascript-coder.com/html-form/javascript-form-validation.phtml
+// for details
+var frmvalidator  = new Validator("myemailform");
+frmvalidator.addValidation("name","req","Please provide your name"); 
+frmvalidator.addValidation("email","req","Please provide your email"); 
+frmvalidator.addValidation("email","email","Please enter a valid email address"); 
+</script>
+                
+</div>
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+    <div class="sidebar one_quarter"> 
+      <!-- ################################################################################################ -->
+ <!--     <h6>Lorem ipsum dolor</h6>
+      <nav class="sdb_holder">
+        <ul>
+          <li><a href="#">Navigation - Level 1</a></li>
+          <li><a href="#">Navigation - Level 1</a>
+            <ul>
+              <li><a href="#">Navigation - Level 2</a></li>
+              <li><a href="#">Navigation - Level 2</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Navigation - Level 1</a>
+            <ul>
+              <li><a href="#">Navigation - Level 2</a></li>
+              <li><a href="#">Navigation - Level 2</a>
+                <ul>
+                  <li><a href="#">Navigation - Level 3</a></li>
+                  <li><a href="#">Navigation - Level 3</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="#">Navigation - Level 1</a></li>
+        </ul>
+      </nav>-->
+
+  <!--    <div class="sdb_holder">
+        <article>
+          <h6>Lorem ipsum dolor</h6>
+          <p>Nuncsed sed conseque a at quismodo tris mauristibus sed habiturpiscinia sed.</p>
+          <ul>
+            <li><a href="#">Lorem ipsum dolor sit</a></li>
+            <li>Etiam vel sapien et</li>
+            <li><a href="#">Etiam vel sapien et</a></li>
+          </ul>
+          <p>Nuncsed sed conseque a at quismodo tris mauristibus sed habiturpiscinia sed. Condimentumsantincidunt dui mattis magna intesque purus orci augue lor nibh.</p>
+          <p class="more"><a href="#">Continue Reading &raquo;</a></p>
+        </article>
+      </div>
+      <!-- ################################################################################################ -->
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- Footer Background Image Wrapper -->
+<div class="bgded overlay" style="background-image:url('images/demo/backgrounds/04.png');"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row4">
+    <footer id="footer" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="btmspace-50 center">
+        <h2 class="heading">Veer Transport</h2>
+         <p> 2222 Bluff Rd, Indianapolis, IN 46225</p>
+
+      </div>
+      <ul class="nospace group">
+        <li class="one_quarter first">
+          <div class="infobox"><i class="fa fa-phone"></i>
+            <ul class="nospace">
+              <li>(317)442-6270</li>
+              <li>(317)442-4030</li>
+            </ul>
+          </div>
+        </li>
+        <li class="one_quarter">
+          <div class="infobox"><i class="fa fa-envelope-o"></i>
+            <ul class="nospace">
+              <li><a href="">Gary@veertransport.com</a></li>
+              <li><a href="">Sam@veertransport.com</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="one_quarter">
+          <div class="infobox"><i class="fa fa-clock-o"></i>
+            <ul class="nospace">
+              <li>Mon - Fri: 8 to 5:00pm</li>
+            </ul>
+          </div>
+        </li>
+        <li class="one_quarter">
+          <div class="infobox"><i class="fa fa-support"></i>
+            <ul class="nospace">
+              <li><a href="#">Veer Transport INC.</a></li>
+              <li><a href="#">Safeway Truck And Trailer Parking</a></li>
+                
+            </ul>
+          </div>
+        </li>
+      </ul>
+      <!-- ################################################################################################ -->
+    </footer>
+  </div>
+  
+  <!-- ################################################################################################ -->
+  <nav class="quicklinks row4">
+    <ul class="hoc clear">
+      <li><a href="index.html"><i class="fa fa-lg fa-home"></i></a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="#footer">Contact</a></li>
+      <li><a href="https://intelliapp.driverapponline.com/c/277514?uri_b=ia_277514_1476142778">Apply</a></li>
+
+    </ul>
+  </nav>
+  
+  <!-- ################################################################################################ -->
+  <div class="wrapper row5">
+    <div id="copyright" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="index.html">Veer Transport INC.</a></p>
+     <!-- <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Footer Background Image Wrapper -->
+<!-- ################################################################################################ -->
+
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="./layout/scripts/jquery.min.js"></script>
+<script src="./layout/scripts/jquery.backtotop.js"></script>
+<script src="./layout/scripts/jquery.mobilemenu.js"></script>
+<script language= "JavaScript" src="./layout/scripts/gen_validatorv31.js" type="text/javascript"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>VEER TRANSPORT INC.</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear">    
+<!-- ################################################################################################ -->   
+    <div class="fl_left">
+      <ul class="nospace">
+        <li><i class="fa fa-phone"></i> (317)534-2628 </li>
+        <li><i class="fa fa-envelope-o"></i> gary@veertransport.com </li>
+      </ul>
+    </div>
+      
+<div class="fl_right">
+      <ul class="nospace">
+        <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="#footer">Contact</a></li>
+        <li><a href="form/driverApplication.pdf" target = "_blank">Apply</a></li> 
+        <!--   <li><a href="#">Thing</a></li> -->
+      </ul>
+</div>
+    </div>
+        </div>
+
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay" style="background-image:url('images/demo/backgrounds/01.png');"> 
+  <!-- ################################################################################################ -->
+
+<div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+
+      <div id="logo" class="fl_left">
+        <h1><a href="index.html"><img src="./images/veerlogo.png" style="width:300px;height:200px;" hspace="2"></a></h1>
+      </div>
+
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="index.html">Home</a></li>
+        <!--   <li><a class="drop" href="#">Forms</a>
+            <ul>
+                 <li><a href="#">Coming Soon</a></li>
+                  
+          <!--    <li><a href="pages/gallery.html">Gallery</a></li> <!-- 
+          <!--    <li><a href="pages/full-width.html">Full Width</a></li> -->
+           <!--   <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>-->
+           <!--   <li><a href="pages/sidebar-right.html">Sidebar Right</a></li> -->
+            <!--  <li><a href="pages/basic-grid.html">Basic Grid</a></li>-->
+            <!-- </ul>
+          </li>
+          
+        <!-- <li><a class="drop" href="#">Dropdown</a>
+            <ul>
+              <li><a href="#">Level 2</a></li>
+              <li><a class="drop" href="#">Level 2 + Drop</a>
+                <ul>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Level 2</a></li>
+            </ul>
+          </li>-->
+          <li><a href="equipment.html">Equipment</a></li>
+          <li><a href="about.html">About</a></li>  
+          <li><a href="formpage.php">Quote</a></li>
+          <li><a href="https://intelliapp.driverapponline.com/c/277514?uri_b=ia_277514_1476142778">Application</a></li>
+        </ul>
+      </nav>
+      <!-- ####################pages/gallery.html############################################################################ -->
+    </header>
+  </div>
+
+  <!-- ################################################################################################ -->
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="flexslider basicslider">
+      <ul class="slides">
+        <li>
+          <article>
+            <p>Veer Transport INC.</p>
+            <h4 class="heading">Welcome to Veer Transport INC.</h4>
+            <footer><a class="btn" href="https://intelliapp.driverapponline.com/c/277514?uri_b=ia_277514_1476142778">Apply</a></footer>
+          </article>
+        </li>
+        <li>
+          <article>
+            <p></p>
+            <h3 class="heading">Wanted Drivers</h3>
+            <p1>Veer Transport is seeking company drivers and owner operators all over the country. Drivers who are persistent and dedicated to drive and deliver on time. If you are interested in becoming a driver for our team please apply directly through our website or call us for more information.</p1>
+            <footer><a class="btn" href="https://intelliapp.driverapponline.com/c/277514?uri_b=ia_277514_1476142778">Apply</a></footer>
+          </article>
+        </li>
+        <li>
+          <article>
+            <p></p>
+            <h3 class="heading">Contact Us</h3>
+            <p>Feel free to contact us for any further information</p>
+            <footer><a class="btn" href="#footer">Contact</a></footer>
+          </article>
+        </li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+
+<!-- End Top Background Image Wrapper -->
+
+<div class="wrapper row3">
+ <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+  <div class="center btmspace-80">
+      <h3 class="heading">Veer Transport INC.</h3>
+      <p class="nospace"> More Content Coming Soon </p>
+      <footer><a href="about.html">Read More &raquo;</a></footer>
+    </div>
+
+    </main>
+    </div>
+    <!-- ################################################################################################ -->
+    
+    
+    
+    
+    
+    <!-- / main body 
+    <div class="clear"></div>
+  </main> -->
+
+<!-- ################################################################################################ -->
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/02.png');">
+  <article class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="three_quarter first">
+      <h6 class="nospace">What Is Veer Transport?</h6>
+      <p class="nospace"> Learn More About This Company </p>
+    </div>
+    <footer class="one_quarter"><a class="btn" href="about.html">More Information &raquo;</a></footer>
+    <!-- ################################################################################################ -->
+  </article>
+</div>
+<!-- ################################################################################################ -->
+<!--<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/03.png');">
+  <article class="hoc container center"> 
+    <!-- ################################################################################################ 
+    <i class="block btmspace-50 fa fa-4x fa-street-view"></i>
+    <h6>this is our location</h6>
+    <p class="btmspace-50">the address can go here with the background image</p>
+    <footer><a class="btn inverse" href="#">go to contact page</a></footer>
+    <!-- ################################################################################################ 
+  </article>
+</div>-->
+<!-- ################################################################################################ -->
+<div class="wrapper row3" style="text-align:center;border:1px solid red">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="btmspace-50 center">
+      <h2>Location</h2>
+      <p>2222 Bluff Rd, Indianapolis, IN 46225 </p>
+    </div>
+
+<p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d641.4262684501471!2d-86.16387188275368!3d39.73657215803831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886b5a1713af9eaf%3A0x8d4635900d2711c7!2s2222+Bluff+Rd%2C+Indianapolis%2C+IN+46225!5e0!3m2!1sen!2sus!4v1522189767019" width="900" height="400" frameborder="0" style="border:5" allowfullscreen></iframe> 
+</p>
+    <!-- ################################################################################################ -->
+  </section>
+</div>
+<!-- ################################################################################################ -->
+
+<!-- Footer Background Image Wrapper -->
+<div class="bgded overlay" style="background-image:url('images/demo/backgrounds/04.png');"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row4">
+    <footer id="footer" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="btmspace-50 center">
+        <h2 class="heading">Veer Transport</h2>
+        <p> 2222 Bluff Rd, Indianapolis, IN 46225</p>
+
+      </div>
+      <ul class="nospace group">
+        <li class="one_quarter first">
+          <div class="infobox"><i class="fa fa-phone"></i>
+            <ul class="nospace">
+              <li>(317)442-6270</li>
+              <li>(317)442-4030</li>
+            </ul>
+          </div>
+        </li>
+        <li class="one_quarter">
+          <div class="infobox"><i class="fa fa-envelope-o"></i>
+            <ul class="nospace">
+              <li><a href="Gary@veertransport.com">Gary@veertransport.com</a></li>
+              <li><a href="Sam@veertransport.com">Sam@veertransport.com</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="one_quarter">
+          <div class="infobox"><i class="fa fa-clock-o"></i>
+            <ul class="nospace">
+              <li>Mon - Fri: 8:00am to 5:00pm</li>
+              <li></li>
+            </ul>
+          </div>
+        </li>
+        <li class="one_quarter">
+          <div class="infobox"><i class="fa fa-support"></i>
+            <ul class="nospace">
+              <li><a href="#">Veer Transport INC.</a></li>
+              <li><a href="#">Safeway Truck And Trailer Parking</a></li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+      <!-- ################################################################################################ -->
+    </footer>
+  </div>
+  
+  <!-- ################################################################################################ -->
+  <nav class="quicklinks row4">
+    <ul class="hoc clear">
+      <li><a href="index.html"><i class="fa fa-lg fa-home"></i></a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="#footer">Contact</a></li>
+      <li><a href="https://intelliapp.driverapponline.com/c/277514?uri_b=ia_277514_1476142778">Apply</a></li>
+    </ul>
+  </nav>
+  
+  <!-- ################################################################################################ -->
+  <div class="wrapper row5">
+    <div id="copyright" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="index.html">Veer Transport INC.</a></p>
+     <!-- <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Footer Background Image Wrapper -->
+<!-- ################################################################################################ -->
+
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<script src="layout/scripts/jquery.flexslider-min.js"></script>
+</body>
+</html>
